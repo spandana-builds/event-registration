8ccdcc49e0076f25cbea6b44828f20febdfd2ffe
spandana-builds <spandana2400@gmail.com>
2026-01-20 23:10:06 +0530

    Deploy-ready backend

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/eventDB")
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -73,7 +76,8 @@ app.get("/registrations/csv", async (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
